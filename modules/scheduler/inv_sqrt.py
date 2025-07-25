30ac29737f421bc73a0d9f83d05c6266262c2c21
Tianyi Liu <tianyi.liu@student.unsw.edu.au>
2025-07-25 10:04:42 +1000

    lr

--- modules/scheduler/inv_sqrt.py
+++ modules/scheduler/inv_sqrt.py
@@ -5,11 +5,12 @@ from paddle.optimizer.lr import LRScheduler
 class InverseSquareRootScheduler(LRScheduler):
     def __init__(self, learning_rate: float, warmup_steps: int, last_epoch: int = -1):
         self.warmup_steps = warmup_steps
+        self.base_lr = learning_rate
         super(InverseSquareRootScheduler, self).__init__(learning_rate, last_epoch)
     
     def get_lr(self):
         step = self.last_epoch + 1
         if step <= self.warmup_steps:
-            return self.base_lrs
+            return self.base_lr
         scale_factor = (self.warmup_steps ** 0.5) / (step ** 0.5)
-        return [base_lr * scale_factor for base_lr in self.base_lrs]
+        return self.base_lr * scale_factor
