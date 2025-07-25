72012fc9a86271afa1723cdfd51e54264fa9fbf3
Tianyi Liu <tianyi.liu@student.unsw.edu.au>
2025-07-25 12:51:14 +1000

    new

--- modules/model.py
+++ modules/model.py
@@ -189,7 +189,7 @@ class EncoderDecoderRetrievalModel(nn.Layer):
             
             # Create batch indices for gather operation
             batch_indices = paddle.arange(B).unsqueeze(1).tile([1, n_top_k_candidates])
-            sampled_log_probas = paddle.log(probas_batched[batch_indices, samples_batched]).reshape([B, -1])
+            sampled_log_probas = paddle.log(paddle.gather(probas_batched, samples_batched, axis=1)).reshape([B, -1])
             samples = samples_batched.reshape([B, -1])
 
             # Get top-K:
